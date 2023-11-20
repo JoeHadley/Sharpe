c21368181c2ebf2e4527a7447854480ac52ba0c0
TheStuffGuy <43622096+TheStuffGuy@users.noreply.github.com>
2023-11-20 15:39:49 +0000

    Update Sharpe.cpp
    Got output data for histograms

--- Sharpe.cpp
+++ Sharpe.cpp
@@ -5,6 +5,43 @@
 #include <cmath>
 using namespace std;
 
+
+template <typename T>
+class WritePyData {
+public:
+    std::string filename = "m_temp.py"; // temporary name of the data file
+
+    WritePyData(const std::string& x, bool initialise = true)
+        : filename(x)
+    {
+        std::ofstream myfile;
+        if (initialise) {
+            myfile.open(filename);
+            myfile << "import numpy as np" << "\n" << "\n";
+        }
+        myfile.close();
+    }
+
+    void write_out_vector(const std::string& array_name, const std::vector<T>& array)
+    {
+        std::ofstream myfile;
+        myfile.open(filename, std::ios_base::app);
+
+        myfile << array_name << " = np.array((" << "\n";
+
+        for (auto i = 0; i < array.size(); ++i) {
+            if (i != (array.size() - 1)) {
+                myfile << array.at(i) << ", " << "\n";
+            } else {
+                myfile << array.at(i) << "\n";
+            }
+        }
+
+        myfile << "))" << "\n";
+        myfile.close();
+    }
+};
+
 // Class for weapons. Internal variables are firingSpeed, firingHeight and shotSpread
 class weapon{
    
@@ -14,11 +51,11 @@ class weapon{
     double firingHeight;
     double shotSpread;
 
-    // Constructor sets the weapon's stats
+// Constructor sets the weapon's stats
     weapon(double v0, double h1, double sigma){
         firingSpeed = v0;
         firingHeight = h1;
-        // Shot Spread is given in degrees, but internally we want to use radians:
+// Shot Spread is given in degrees, but internally we want to use radians:
         shotSpread = sigma*M_PI/180;
     }
 
@@ -34,42 +71,12 @@ class weapon{
     }
 };
 
-void rnum(double& input, opt_angle){ //gaussian distribution for the angle with standard deviation  3 degrees (does this need to be in radians?)
-      const double mean = opt_angle;
-      const double standdev = 3;
-      random_device rand_dev;
-      mt19937 gen(rand_dev());
-      normal_distribution<double> distr(mean, standdev);
-      input = distr(gen);
-}
-double average(vector<double> Q, double z){ //code for finding the average of a vector
-   int N = Q.size();
-   
-   for (int i = 0; i < N; ++i)//change 1000 to the no. of shots 
-    {
-        z = z+Q[i];
-    }
-    double av = z/N;
-    cout << "average is " << av;
-    return av;
-}
 
-double stand_dev(vector<double> Q, double var, double mean){ //code for finding the standard deviation of a vector
 
-   int N = Q.size();
-   for (int i = 0; i < N; ++i)//change 1000 to the no. of shots 
-    {
-       var = var + pow(Q[i]-mean,2)/N;
-    }
-   double sd = sqrt(var)
-   cout << "standard deviation is " << sd;
-   return sd;
-   
-}
 
 
 // Function that runs a hypothetical fireShot() function 15 times and gathers the outputs in a vector 
-vector<double> doTrial(weapon gun, double distance, double targetLowerBound, double targetUpperBound){
+/*vector<double> doTrial(weapon gun, double distance, double targetLowerBound, double targetUpperBound){
 
     int shotNumber = 15;
     vector<double> shotLocations = {};
@@ -81,24 +88,67 @@ vector<double> doTrial(weapon gun, double distance, double targetLowerBound, dou
         /*
         double y_value = fireShot(weapon gun, double distance);
         shotLocations.push_back(y_value);
-        */
+        
     }
     
+}*/
+
+//code for random number distribution
+
+double random_number(){
+
+
+random_device rand_dev;
+mt19937 generator(rand_dev());
+normal_distribution<double> distr(0, 0.5*M_PI/(double)180);
+return distr(generator);
 }
+
+
+// finding the height of the hit
+
+double actualHeight( double inputAngle)
+{   
+    
+    double height;
+    double initial_height = 0.7; //dont know syntax to call this from the class
+    double g =9.81;
+    double v_0 = 450;
+    double d =100;
+
+    height = initial_height - g*d*d/(2*v_0*v_0) + d*tan(inputAngle) -g*d*d*tan(inputAngle)*tan(inputAngle)/(2*v_0*v_0);  
+    return height;
+}
+    
+
 
 
 // A function that takes the array of shot locations from a trial and counts how many hit the target.
 int countHits(vector<double> shotArray,double targetLowerBound, double targetUpperBound){
     
-    int hitCount = 0;
+    vector <int> hit;
     for (int i = 0; i<shotArray.size(); i++){
 
         if (shotArray[i] <= targetUpperBound and shotArray[i] >= targetLowerBound){
-            hitCount++;
+            hit.push_back(i);
         }
     }   
-    return hitCount;
+    return hit.size();
 }
+ double doTrial(double thetaC){   
+
+    vector <double> trial; 
+    
+
+    for(int i=0; i <=15; i++)
+    {    
+         double shootingAngle = thetaC + random_number();         
+         trial.push_back(actualHeight(shootingAngle));                
+
+    }
+    return countHits(trial,0.3,2.3);  
+    
+    }
 
 
 
@@ -126,6 +176,48 @@ int main(){
     // Find optimal angle
     double thetaC = musket.findOptimalAngle(distance, targetCentre, g);
     cout << thetaC;
+
+    //finding the shooting angle
+
+   
+
+    
+
+    //setting up the shot array test
+
+   
+
+    
+    vector <int>  trialHits;
+
+    for(int i=0; i<= 1000; i++)
+    {
+        trialHits.push_back(doTrial(thetaC));
+
+
+    }
+
+  
+   
+
+   /* vector <int> size_quartile;
+    
+
+    size_quartile.push_back( countHits(ShotArray,0.3,0.7));
+    size_quartile.push_back( countHits(ShotArray,0.7,1.1));
+    size_quartile.push_back( countHits(ShotArray,1.1,1.5));
+    size_quartile.push_back( countHits(ShotArray,1.5,1.9));
+    size_quartile.push_back( countHits(ShotArray,1.9,2.3));
+    */
+    
+
+    WritePyData<int> writer("hitCounts.py");
+
+    string name = "hitCounts";
+
+    writer.write_out_vector("my_array", trialHits );    
+    
+
 
 
     return 0;
